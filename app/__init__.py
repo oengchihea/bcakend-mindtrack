081f560446b2721d6f2947ef764b15e615e33ea4
oengchihea <chihea.oeng22@kit.edu.kh>
2025-06-22 01:28:24 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -96,3 +96,6 @@ else:
     # For Vercel WSGI
     from werkzeug.middleware.proxy_fix import ProxyFix
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+# Explicitly export the app for WSGI
+application = app
